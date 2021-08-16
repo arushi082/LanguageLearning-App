adfea4cee015fb21da0096915ea6687e16251c2e
soransh <57911107+soransh-singh@users.noreply.github.com>
2021-08-16 22:34:57 +0530

    update
    corrected links in header.html

--- Language/Login/urls.py
+++ Language/Login/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path, include
 from.import views
 
+app_name = "Login"
+
 urlpatterns = [
     path("", views.homepage, name="homepage"),
     path("register", views.register_request, name="register"),
--- Language/Translate/urls.py
+++ Language/Translate/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path, include
 from .import views
 
-app_name = "translate"
+app_name = "Translate"
 
 urlpatterns = [
     #translate
