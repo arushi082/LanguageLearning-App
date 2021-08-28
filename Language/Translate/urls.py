f87325b67a8a552396f1bc364b15f19cc746e5fe
soransh <57911107+soransh-singh@users.noreply.github.com>
2021-08-28 15:15:44 +0530

    update

--- Language/Translate/urls.py
+++ Language/Translate/urls.py
@@ -8,15 +8,3 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("translate/", views.translate, name="translate")
     ]
-
-
-"""
-app_name = "Dictionary"
-
-urlpatterns = [
-    #dictionary-page
-    path("", views.index, name="index"),
-    path("dictionary/", views.dictionary, name="dictionary")
-    ]
-
-"""
--- Language/Translate/views.py
+++ Language/Translate/views.py
@@ -16,34 +16,3 @@ def translate(request):
         "translation": translation
     }
     return JsonResponse(context)
-
-"""
-def translate(request):
-    if request.method == "POST":
-        text = request.POST["translate"]
-        language = request.POST["language"]
-        translator= Translator(to_lang=language)
-        translation = translator.translate(text)
-        return HttpResponse(translation)
-        return render(request, "translate/translate.html")
-"""
-
-"""
-
-
-
-# view for dictionary page
-def dictionary(request):
-    search = request.GET.get('search')
-    dictionary = PyDictionary
-    items = []
-    meaning = dictionary.meaning(search)
-    synonyms = dictionary.synonym(search)
-    antonyms = dictionary.antonym(search)
-    context = {
-        'meaning': meaning,
-        'synonyms': synonyms,
-        'antonyms': antonyms
-    }
-    return JsonResponse(context)
-"""
